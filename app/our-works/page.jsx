df23f4f27b41d95fc3a924191760231ec3b3d870
Arunachaladinesh K <arunachala@seclogic.io>
2024-07-28 20:47:03 +0530

    update: build conflict fix

--- app/our-works/page.jsx
+++ app/our-works/page.jsx
@@ -1,38 +1,38 @@
-import { Tabs } from "@/components/our-works-components/ourWorksTab";
+// import { Tabs } from "@/components/our-works-components/ourWorksTab";
 
-let tabs = [
-  {
-    value: "graphic-design",
-    title: "Graphic Design",
-    content: (
-      <>
-        <p>test</p>
-      </>
-    ),
-  },
-  {
-    value: "web-dev-service",
-    title: "Website Development Service",
-    content: (
-      <>
-        <p>test</p>
-      </>
-    ),
-  },
-  {
-    value: "app-service",
-    title: "APP / SAAS Development Service",
-    content: (
-      <>
-        <p>test</p>
-      </>
-    ),
-  },
-];
+// let tabs = [
+//   {
+//     value: "graphic-design",
+//     title: "Graphic Design",
+//     content: (
+//       <>
+//         <p>test</p>
+//       </>
+//     ),
+//   },
+//   {
+//     value: "web-dev-service",
+//     title: "Website Development Service",
+//     content: (
+//       <>
+//         <p>test</p>
+//       </>
+//     ),
+//   },
+//   {
+//     value: "app-service",
+//     title: "APP / SAAS Development Service",
+//     content: (
+//       <>
+//         <p>test</p>
+//       </>
+//     ),
+//   },
+// ];
 const OurWorks = () => {
   return (
     <section className="common-pricing-page-wrapper">
-      <Tabs tabs={tabs} />
+      {/* <Tabs tabs={tabs} /> */}
     </section>
   );
 };
